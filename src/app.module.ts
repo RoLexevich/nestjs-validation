3de6aaf49ca559f058dc8036b10de8c60a6e13d9
Родионов Алексей Игоревич <rodionov_a@tii.ru>
2023-11-19 17:38:09 +0300

    Initial

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookService } from './book/book.service';
+import { BookController } from './book/book.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+import "dotenv/config";
+
+
+@Module({
+  imports: [MongooseModule.forRoot(`${process.env.MONGODB_CONNECTION_STRING}`), BookModule, ],
+  controllers: [AppController ],
+  providers: [AppService],
+})
+export class AppModule {}
